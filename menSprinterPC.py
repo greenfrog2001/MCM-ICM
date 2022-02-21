dba8a3463307a9237898e649200b9e866a06f978
greenfrog2001 <phanviettan1@gmail.com>
2022-02-21 22:45:12 +0700

    Update time axis and Average data

--- menSprinterPC.py
+++ menSprinterPC.py
@@ -3,14 +3,21 @@ from scipy.optimize import curve_fit
 import numpy as np
 
 def func(x, a, b):
-    return a * np.exp(-b * x) + 6.40
+    return a * np.exp(-b * x) + CP
 
 #a = 45, b = 0.5
 
-times = [5, 60, 300, 1200]
+times = [5, 60, 300, 3600]
 log_times = np.log(times)
 xdata = log_times
+
+# # World class
 ydata = np.array([24.04, 11.05, 7.60, 6.40])
+CP = 6.40
+
+# Average
+# ydata = np.array([15.88, 8.05, 4.50, 3.73])
+# CP = 3.73
 
 plt.scatter(xdata, ydata, color = 'r')
 
@@ -18,10 +25,15 @@ popt, pcov = curve_fit(func, xdata, ydata)
 
 plt.plot(xdata, func(xdata, *popt), 'r', label='Power Curve: a=%5.3f, b=%5.3f' % tuple(popt))
 
-plt.axhline(y = 6.40, color = 'b', linestyle = '--', label = 'Critical Power: 6.40')
-
+plt.axhline(y = CP, color = 'b', linestyle = '--', label = f'Critical Power: {CP}')
+plt.gca().set_ylim([0, 30])
 plt.xlabel('$Log_{10}$ of Time (seconds)')
 plt.ylabel('Power (W/kg)')
+
+# World class title
 plt.title(label = "Men's Word Champion Sprinter")
+
+# Avergae title
+# plt.title(label = "Men's Average Sprinter")
 plt.legend()
 plt.show()
