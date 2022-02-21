2c1779b459de9f1a1af069725655bad9a616d826
greenfrog2001 <phanviettan1@gmail.com>
2022-02-21 22:45:17 +0700

    Update time axis and Average data

--- womenSprinterPC.py
+++ womenSprinterPC.py
@@ -1,16 +1,23 @@
-from cv2 import log
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 
 def func(x, a, b):
-    return a * np.exp(-b * x) + 5.69
+    return a * np.exp(-b * x) + CP
 
-times = np.array([5, 60, 300, 1200])
-times = [5, 60, 300, 1200]
+#a = 45, b = 0.5
+
+times = [5, 60, 300, 3600]
 log_times = np.log(times)
 xdata = log_times
-ydata = np.array([19.42, 9.29, 6.61, 5.69])
+
+# World class
+# ydata = np.array([19.42, 9.29, 6.61, 5.69])
+# CP = 5.69
+
+# Average
+ydata = np.array([12.95, 6.57, 3.83, 3.23])
+CP = 3.23
 
 plt.scatter(xdata, ydata, color = 'r')
 
@@ -18,10 +25,15 @@ popt, pcov = curve_fit(func, xdata, ydata)
 
 plt.plot(xdata, func(xdata, *popt), 'r', label='Power Curve: a=%5.3f, b=%5.3f' % tuple(popt))
 
-plt.axhline(y = 5.69, color = 'b', linestyle = '--', label = 'Critical Power: 5.69')
-
+plt.axhline(y = CP, color = 'b', linestyle = '--', label = f'Critical Power: {CP}')
+plt.gca().set_ylim([0, 30])
 plt.xlabel('$Log_{10}$ of Time (seconds)')
-plt.ylabel('Power (W)')
-plt.title(label = "Women's Word Champion Sprinter")
+plt.ylabel('Power (W/kg)')
+
+# World class title
+# plt.title(label = "Women's Word Champion Sprinter")
+
+# Avergae title
+plt.title(label = "Women's Average Sprinter")
 plt.legend()
 plt.show()
