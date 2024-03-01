2cc054b8826dbf6931071573d5a11a52b0496b17
greenfrog2001 <phanviettan1@gmail.com>
2024-03-01 23:32:47 +0700

    Update plot

--- menSprinterPC.py
+++ menSprinterPC.py
@@ -12,8 +12,8 @@ log_times = np.log(times)
 xdata = log_times
 
 # # World class
-ydata = np.array([24.04, 11.05, 7.60, 6.40])
-CP = 6.40
+ydata = np.array([24.04*70, 11.05*70, 7.60*70, 6.40*70])
+CP = 6.40*70
 
 # Average
 # ydata = np.array([15.88, 8.05, 4.50, 3.73])
@@ -26,7 +26,7 @@ popt, pcov = curve_fit(func, xdata, ydata)
 plt.plot(xdata, func(xdata, *popt), 'r', label='Power Curve: a=%5.3f, b=%5.3f' % tuple(popt))
 
 plt.axhline(y = CP, color = 'b', linestyle = '--', label = f'Critical Power: {CP}')
-plt.gca().set_ylim([0, 30])
+# plt.gca().set_ylim([0, 30])
 plt.xlabel('$Log_{10}$ of Time (seconds)')
 plt.ylabel('Power (W/kg)')
 
@@ -35,5 +35,9 @@ plt.title(label = "Men's Word Champion Sprinter")
 
 # Avergae title
 # plt.title(label = "Men's Average Sprinter")
+
+# Clear xticks
+plt.gca().axes.get_xaxis().set_ticks([])
+
 plt.legend()
 plt.show()
